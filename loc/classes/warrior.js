39a7d3f149e6b00a439090c498c97a838dfed1aa
Rodrigochacon <30390830+Rodrigochacon@users.noreply.github.com>
2019-04-14 16:30:37 -0300

    Update warrior.js
    Brazilian Portuguese

--- loc/classes/warrior.js
+++ loc/classes/warrior.js
@@ -7,7 +7,8 @@ export default {
     ru: 'Воин',
     es: 'Soldado',
     fr: 'Guerrier',
-    hi: 'योधा'
+    hi: 'योधा',
+    ptbr: 'Guerreiro'
   },
   // Class description, visible during character creation
   description: {
@@ -17,6 +18,7 @@ export default {
     de: 'Krieger sind so gebaut, dass sie mehr Monster als jede andere Klasse besiegen, indem sie eine hohe Verteidigung durch Ausrüstung (Schilde) und starke Verteidigungszauber haben.',
     es: 'Los tanques de la guerra, soldados luchan para el honor y gloria de la batalla. Ellos pueden soportar una gran cantidad de daño con sus escudos y buffs de defensa.',
     fr: 'Les guerriers sont construits pour chasser plus de monstres que n\'importe quelle autre classe en ayant une haute défense à travers l\'équipement (boucliers) et des buffs défensifs forts.',
-    hi: 'सर्वश्रेष्ठ रक्षा उपकरण और ताक़तवर कवच के उपयोग से सारी श्रेणियो में योधा सबसे अधिक मार रोक सकते हैं।'
+    hi: 'सर्वश्रेष्ठ रक्षा उपकरण और ताक़तवर कवच के उपयोग से सारी श्रेणियो में योधा सबसे अधिक मार रोक सकते हैं।',
+    ptbr: 'Guerreiros são feitos para tankar mais monstros do que outras classes possuindo alta defesa a base de equipamentos (Escudos) e fortes buffs defensivos.'
   }
 }
