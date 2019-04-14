cf8e829c9f3b67ac254c98661ee3f64e6c9a45d2
Rodrigochacon <30390830+Rodrigochacon@users.noreply.github.com>
2019-04-14 16:23:21 -0300

    Update mage.js
    Brazilian Portuguese

--- loc/classes/mage.js
+++ loc/classes/mage.js
@@ -6,15 +6,18 @@ export default {
     se: 'Magiker',
     es: 'Mago',
     hi: 'जादूगर',
-    fr: 'Mage'
+    fr: 'Mage',
+    ptbr: 'Mago'
   },
   // Class description, visible during character creation
   description: {
     en: 'Mages deal very heavy large scale Area of Effect damage while also providing utility through slowing targets with their ice based magic.',
     se: 'Magiker gör mycket hög areal skada och har även hjälp av sin is baserade magi för att kunna göra sina motståndare långsamma.',
+    fr: 'Les mages infligent des dégats de zone à grande échelle tout en ralentissant leurs ennemis grâce à leur magie basée sur la glace.',
     de: 'Magier verursachen sehr großen Flächenschaden und sind gleichzeitig nützlich, da sie Ziele mit ihrer auf Eis basierenden Magie verlangsamen.',
     es: 'Los magos usan los poderes del invierno para ralentizar y hacer daño massivo a sus enemigos.',
     hi: 'जादूगर बड़े स्तर पर एक ईलाके में नुक़सान पहुँचा सकते है और साथ ही साथ अपें बर्फ़ीले जादू से दुश्मनो को धीमे भी कर सकते है।',
-    fr: 'Les Mages infligent de très lourds dégâts à grande échelle de la Zone d\'Effet tout en offrant de l\'utilité en ralentissant les cibles grâce à leur magie de glace.'
+    fr: 'Les Mages infligent de très lourds dégâts à grande échelle de la Zone d\'Effet tout en offrant de l\'utilité en ralentissant les cibles grâce à leur magie de glace.',
+    ptbr: 'Magos dão altos danos em áreas enquanto providenciam suporte fazendo seus inimigos lentos usando os poderes do gelo.'
   }
 }
