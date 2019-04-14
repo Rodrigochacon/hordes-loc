2454b21bab5e3b7dfe2483b84620b9af6764ff74
Rodrigochacon <30390830+Rodrigochacon@users.noreply.github.com>
2019-04-14 16:15:44 -0300

    Update archer.js
    Brazilian Portuguese

--- loc/classes/archer.js
+++ loc/classes/archer.js
@@ -7,7 +7,8 @@ export default {
     ru: 'Лучник',
     ko: '궁수',
     es: 'Arquero',
-    fr: 'Archer'
+    fr: 'Archer',
+    ptbr: 'Arqueiro'
   },
   // Class description, visible during character creation
   description: {
@@ -17,6 +18,7 @@ export default {
     ru: 'Лучники наносят высокий урон по одиночной цели и взрывной урон по области. Хороший лучник всегда будет держать дистанцию и отстреливать врагов издалека.',
     ko: '궁수는 높은 단일 타겟 데미지와 버스트 에어리어 피해를줍니다. 좋은 궁수는 먼 거리에서 항상 적을 공격합니다.',
     es: 'Los arqueros hacen daño increíble con sus flechas y pueden disparar muchos flechos a una vez con daño explosivo. La estregia mejor es evitar los enemigos y atacar a lo lejos',
-    fr: 'Les archers infligent des dégâts élevés sur une cible précise et des dégâts de zone importants. Un bon archer garde toujours ses distances et élimine ses ennemis de loin.'
+    fr: 'Les archers infligent des dégâts élevés sur une cible précise et des dégâts de zone importants. Un bon archer garde toujours ses distances et élimine ses ennemis de loin.',
+    ptbr: 'Arqueiros dão alto dano em um alvo e dano em áreas com efeitos em rajadas. Um bom arqueiro sempre manterá sua distancia e atingirá enemigos de longe.'  
   }
 }
